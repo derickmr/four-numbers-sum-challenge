b55e6705df6f86e386eeb22afd159327e3f4a988
Derick Rentz <derickrentz@pop-os.localdomain>
2021-09-07 19:49:29 -0300

    Initial challenge code

---
+++ src/com/taller/challenge/Program.java
@@ -0,0 +1,10 @@
+package com.taller.challenge;
+
+import java.util.*;
+
+class Program {
+    public static List<Integer[]> fourNumberSum(int[] array, int targetSum) {
+        // Write your code here.
+        return new ArrayList<Integer[]>();
+    }
+}
